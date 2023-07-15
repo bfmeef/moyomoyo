7e8f5fc98f9bc6e73384421777b70c28789dddd8
bfmeef <sumin000619@gmail.com>
2023-07-16 05:10:33 +0900

    feat: 하단 바 생성

---
+++ app/src/main/java/com/example/moyomoyo/MainMenuGroup.java
@@ -0,0 +1,22 @@
+package com.example.moyomoyo;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MainMenuGroup extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_main_menu_group,
+                container, false);
+
+        return rootView;
+    }
+}
