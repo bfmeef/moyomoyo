3f9917fef5ee03448ed8f290fba857918e3852ed
bfmeef <sumin000619@gmail.com>
2023-07-24 23:50:24 +0900

    feat: 스터디룸, 소모임 모델 추가

--- server/moyomoyo/groups/models.py
+++ server/moyomoyo/groups/models.py
@@ -1,3 +1,45 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Group(models.Model):
+    group_id = models.UUIDField(primary_key=True,
+                                default=uuid.uuid4,
+                                unique=True,
+                                editable=False)
+    name = models.CharField(max_length=50)
+    info = models.CharField(max_length=50)
+    field = models.CharField(max_length=10)
+    is_reserved = models.BooleanField(default=False)
+
+
+class Member(models.Model):
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
+    user_id = models.ForeignKey()
+    is_leader = models.BooleanField(default=False)
+
+
+class Post(models.Model):
+    post_id = models.UUIDField(primary_key=True,
+                               default=uuid.uuid4,
+                               unique=True,
+                               editable=False)
+    user_id = models.ForeignKey()
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    content = models.CharField(max_length=500)
+    create_date = models.DateTimeField(auto_now_add=True, editable=False)
+    is_notice = models.BooleanField(default=False)
+
+
+class Comment(models.Model):
+    comment_id = models.UUIDField(primary_key=True,
+                                  default=uuid.uuid4,
+                                  unique=True,
+                                  editable=False)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user_id = models.ForeignKey()
+    content = models.CharField(max_length=500)
+    create_date = models.DateTimeField(auto_now_add=True, editable=False)
+    
--- server/moyomoyo/studyrooms/models.py
+++ server/moyomoyo/studyrooms/models.py
@@ -1,3 +1,37 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+class StudyRoom(models.Model):
+    study_room_id = models.UUIDField(primary_key=True,
+                                     default=uuid.uuid4,
+                                     unique=True,
+                                     editable=False)
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    info = models.CharField(max_length=50)
+    capacity = models.IntegerField()
+
+
+class Reservation(models.Model):
+    reservation_id = models.UUIDField(primary_key=True,
+                                      default=uuid.uuid4,
+                                      unique=True,
+                                      editable=False)
+    studyroom = models.ForeignKey(StudyRoom, on_delete=models.CASCADE)
+    reservation_date = models.DateTimeField()
+    return_date = models.DateTimeField()
+    participants = models.IntegerField()
+    is_used = models.BooleanField(default=False)
+    is_returned = models.BooleanField(default=False)
+
+
+class Participant(models.Model):
+    participant_id = models.UUIDField(primary_key=True,
+                                      default=uuid.uuid4,
+                                      unique=True,
+                                      editable=False)
+    reservation_id = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+    user_id = models.ForeignKey()
+    study_room_id = models.ForeignKey(StudyRoom, on_delete=models.CASCADE)
+    is_leader = models.BooleanField(default=False)
