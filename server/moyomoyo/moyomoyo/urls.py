52db911c43cc5dd19e26a39abcdc97e526cd9dd2
bfmeef <sumin000619@gmail.com>
2023-07-26 23:07:32 +0900

    feat: accounts urls 추가

--- server/moyomoyo/moyomoyo/urls.py
+++ server/moyomoyo/moyomoyo/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin', admin.site.urls),
+    path('api/', include([
+        path('accounts/', include('accounts.urls')),
+        path('studyrooms/', include('studyrooms.urls')),
+    ])),
 ]
